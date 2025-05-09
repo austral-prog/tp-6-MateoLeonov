1b991e9234fe58934c3742b74644b16c78e773c6
MateoLeonov <mjleonov@mail.austral.edu.ar>
2025-05-09 10:59:09 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,9 +1,11 @@
 def remove_elements(list_to_remove_elements):
-    list1 = list_to_remove_elements.copy()
-    del list1[5]
-    del list1[4]
-    del list1[0]
-    return list1
+    if len(list_to_remove_elements) >= 6:
+        del list_to_remove_elements[5]
+    if len(list_to_remove_elements) >= 5:
+        del list_to_remove_elements[4]
+    if len(list_to_remove_elements) >= 1:
+        del list_to_remove_elements[0]
+    return list_to_remove_elements
 
 def add_elements(list_to_add_elements):
     list2 = list_to_add_elements.copy()
@@ -16,10 +18,14 @@ def is_empty(list_to_check):
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    if list_to_compare1[2] == list_to_compare2[2]:
-        return True
+    if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3:
+        if list_to_compare1[2] == list_to_compare2[2]:
+            return True
+        else:
+            return False
     else:
         return False
+
 
 def list_of_lists(list_of_lists_to_modify):
     list_of_lists_to_modify[0]= list_of_lists_to_modify[0][:2]
